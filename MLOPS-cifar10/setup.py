7f5d6bbca46d5d5dfc145afd423b7967afa58999
Morsdors <dorota.solarska@student.put.poznan.pl>
2023-09-27 13:14:54 +0200

    First commit

---
+++ MLOPS-cifar10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Image Classification with cifar10 dataset for MLOps course.',
+    author='MLOps-essi-upc',
+    license='MIT',
+)
