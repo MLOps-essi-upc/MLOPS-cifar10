aa2449f044e5aa37229ebf4e7000add606f47a86
Edu <wazee96@gmail.com>
2023-10-18 09:44:35 +0200

    10/10 in pylint for train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,22 +1,20 @@
+"""
+This module contains the code for training a machine learning model.
+It includes calls to functions for load model parameters from yaml file and
+functions to load a generator for train and test the model.
+"""
+import pickle
 from pathlib import Path
 
-import mlflow, time, pickle, sys, yaml
+import mlflow
 
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.keras.applications import InceptionV3, VGG16
-
-from tensorflow.keras.applications.inception_v3 import preprocess_input
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.models import Model
-
+from src.models.utils import read_data_preparation_params, load_dataset
+from src import MODELS_DIR, TRAIN_DATA_DIR
 #from codecarbon import EmissionsTracker
 
-sys.path.append('/home/w4z3/Git/MLOPS-cifar10')
-from src import MODELS_DIR, TRAIN_DATA_DIR
-from utils import *
 
 mlflow.set_experiment("cifar10")
 mlflow.sklearn.autolog(log_model_signatures=False, log_datasets=False)
@@ -26,7 +24,7 @@ with mlflow.start_run():
     input_folder_path = TRAIN_DATA_DIR
 
     # Get preparation parameters
-    params = readDataPreparationParams("train")
+    params = read_data_preparation_params("train")
 
     # `random_state` for the sake of reproducibility for tensorflow and numpy.
     tf.random.set_seed(params["random_state"])
@@ -38,36 +36,35 @@ with mlflow.start_run():
 
     # Specify the model
     if params["algorithm"] == "InceptionV3":
-        algorithm = InceptionV3
-        input_size = (75, 75)
-        input_shape = (75, 75, 3)
-    elif params["algorithm"] == "VGG16":
-        algorithm = VGG16
-        input_size = (32, 32)
-        input_shape = (32, 32, 3)
+        algorithm = tf.keras.applications.InceptionV3
+        input_size = (params["input_size"], params["input_size"])
+        input_shape = (params["input_size"], params["input_size"], 3)
 
     # Get Train dataset as Generator
     train_generator = load_dataset(params, input_folder_path, input_size)
 
     imagenet_base_model = algorithm(weights='imagenet', include_top=False, input_shape=input_shape)
-    
+
     for layer in imagenet_base_model.layers:
         layer.trainable = False
 
-    x = Flatten()(imagenet_base_model.output)
-    x = Dense(128, activation='relu')(x)
-    predictions = Dense(10, activation='softmax')(x)
+    x = tf.keras.layers.Flatten()(imagenet_base_model.output)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    predictions = tf.keras.layers.Dense(10, activation='softmax')(x)
 
-    imagenet_model = Model(inputs=imagenet_base_model.input, outputs=predictions)
-    imagenet_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    
-        
-    imagenet_model.fit(train_generator, epochs=params["num_epochs"], batch_size=params["batch_size"])
+    imagenet_model = tf.keras.Model(inputs=imagenet_base_model.input, outputs=predictions)
+    imagenet_model.compile(optimizer='adam',
+                           loss='categorical_crossentropy',
+                           metrics=['accuracy'])
+
+    imagenet_model.fit(train_generator,
+                       epochs=params["num_epochs"],
+                       batch_size=params["batch_size"])
 
     # Save the model as a pickle file
     Path("models").mkdir(exist_ok=True)
     with open(MODELS_DIR / "imagenet_model_cifar10.pkl", "wb") as pickle_file:
         pickle.dump(imagenet_model, pickle_file)
 
-    # Save the model with TensorFlow methods to ensure compatibility and integrity. 
+    # Save the model with TensorFlow methods to ensure compatibility and integrity.
     imagenet_model.save(MODELS_DIR/'imagenet_model_cifar10.h5')
